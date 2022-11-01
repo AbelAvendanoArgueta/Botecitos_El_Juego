7ca29f1db7521bdcd1f484bde12fbf9af00f2001
Lord_Medusin <jaegerfer7avendano@gmail.com>
2022-10-31 20:21:15 -0600

    ventana ya funciona

--- program_init.py
+++ program_init.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import sys
 
 #####
 # VARIABLES
@@ -15,8 +16,22 @@ pantalla_ancho = 700
     # NO CONSTANTES
     ######
 
+#####
+# FUNCIONES
+#####
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((pantalla_ancho, pantalla_largo))
-    pygame.display.set_caption("Botecitos 'El juego'")
+    Ventana_inicial = pygame.display.set_mode((pantalla_ancho, pantalla_largo))
+    pygame.display.set_caption("Botecitos 'El juego'") 
+
+    Ventana_inicial.fill((0,0,100))
+
+    while 1 == 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+if __name__ == "__main__":
+    main()
+
