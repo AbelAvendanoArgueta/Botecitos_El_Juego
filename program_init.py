6acd2905433246647070ad3db3b8bc021e995cb5
Lord_Medusin <jaegerfer7avendano@gmail.com>
2022-11-07 21:05:34 -0600

    README.md fue creado

--- program_init.py
+++ program_init.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame_gui
 from pygame.locals import *
 import sys
 
@@ -28,9 +29,17 @@ def main():
     fondo_de_pantalla_inicial = pygame.image.load("./recursos/imagenes/fondo_pantalla_inicial.png").convert_alpha()
 
     Ventana_inicial.blit(fondo_de_pantalla_inicial, (0, -100))
-    # se muestran lo cambios en pantalla
-    pygame.display.flip()
+    pygame.display.flip() # se muestran lo cambios en pantalla
     
+    #boton
+
+    manager = pygame_gui.UIManager((800,600))
+    hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350,275),(100,50)),
+                                             text='Say Hello',
+                                             manager=manager)
+
+                            
+
 
     while 1 == 1:
         for event in pygame.event.get():
