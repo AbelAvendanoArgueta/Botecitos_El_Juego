fb2608d5d90f15a83e4657e59e006cc015fe610d
Lord_Medusin <jaegerfer7avendano@gmail.com>
2022-11-08 19:31:17 -0600

    Agregamos tipografia, e importamos la ttf en el codigo

--- program_init.py
+++ program_init.py
@@ -10,8 +10,8 @@ import sys
     ###### 
     # CONSTANTES
     ######
-pantalla_largo = 500
-pantalla_ancho = 700
+pantalla_altura = 550
+pantalla_ancho = 1440
 
     ###### 
     # NO CONSTANTES
@@ -21,30 +21,22 @@ pantalla_ancho = 700
 # FUNCIONES
 #####
 
-def main():
-    pygame.init()
-    Ventana_inicial = pygame.display.set_mode((pantalla_ancho, pantalla_largo))
-    pygame.display.set_caption("Botecitos 'El juego'") 
 
-    fondo_de_pantalla_inicial = pygame.image.load("./recursos/imagenes/fondo_pantalla_inicial.png").convert_alpha()
+pygame.init()
+Ventana_inicial = pygame.display.set_mode((pantalla_ancho, pantalla_altura))
+pygame.display.set_caption("Botecitos 'El juego'") 
 
-    Ventana_inicial.blit(fondo_de_pantalla_inicial, (0, -100))
-    pygame.display.flip() # se muestran lo cambios en pantalla
-    
-    #boton
+fondo_de_pantalla_inicial = pygame.image.load("./recursos/imagenes/fondo_pantalla_inicial.png").convert_alpha()
 
-    manager = pygame_gui.UIManager((800,600))
-    hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350,275),(100,50)),
-                                             text='Say Hello',
-                                             manager=manager)
+Ventana_inicial.blit(fondo_de_pantalla_inicial, (0, -100))
+pygame.display.flip() # se muestran lo cambios en pantalla
 
-                            
+# Codigo para cerrar la ventana                                                    
+while 1 == 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-
-    while 1 == 1:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-if __name__ == "__main__":
-    main()
+# Importacion de Fuente
+def get_font(fuente_T): # fuente_T = Tamaño de la fuente 
+    return pygame.font.Font("./recursos/Tipografia/font.ttf", fuente_T)
