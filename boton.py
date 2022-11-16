cfa2bb73a25be8540859a62ba1068c74f4036b1a
Lord_Medusin <jaegerfer7avendano@gmail.com>
2022-11-15 21:26:58 -0600

    haciendo pruebas

--- boton.py
+++ boton.py
@@ -1,4 +1,4 @@
-class Boton(): #clase boton
+class Boton(): # Clase boton
 	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
 		self.image = image
 		self.x_pos = pos[0]
