eeac8d811b498afe7f703ca56d11c9cf466f59d2
Lord_Medusin <jaegerfer7avendano@gmail.com>
2022-11-24 17:53:43 -0600

    se agrega funcion atravezar_tablero

--- src_pve_logic.py
+++ src_pve_logic.py
@@ -10,4 +10,10 @@ reloj = pygame.time.Clock()
 
 def tomar_fuente(fuente_T): # fuente_T = Tamaño de la fuente 
     return pygame.font.Font(import_fuente, fuente_T) # Se importa la fuente, y se pasa la variable "fuente_T" para definir tamaño de la fuente
-    
+### Dibujo, trazado y analisis de tableros
+def atravezar_tablero(tablero_de_juego):
+    cruzando_tablero = [[' ']*lado for _ in range(lado)]
+    for i in range(lado):
+        for j in range(lado):
+            cruzando_tablero[i][j] = tablero_de_juego[j][i]
+    return cruzando_tablero
